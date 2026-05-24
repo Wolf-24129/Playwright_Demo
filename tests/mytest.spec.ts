fd1a8a5a88e7592552a6afc7eaf4b83328cc841c
Your Name <your.email@example.com>
2026-05-24 18:59:56 +0530

    await

--- tests/mytest.spec.ts
+++ tests/mytest.spec.ts
@@ -14,8 +14,8 @@ test("title",()=>{
 test("Verify page title", async ({ page }) => {
   await page.goto("https://testautomationpractice.blogspot.com//");
 
-  let title: string = await page.title();
-  console.log("Page title is : " + title);
+  // let title: string = await page.title();
+  console.log("Page title is : " + (await page.title()));
 
   await expect(page).toHaveTitle("Automation Testing Practice");
 });
